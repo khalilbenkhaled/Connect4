9682b4f9616e97678b3051312fe0adc09856de60
Khalil <khalilbenkhaled01@gamil.com>
2021-04-28 19:48:22 +0100

    Changing the way the depth function works

--- fonctions.c
+++ fonctions.c
@@ -256,7 +256,7 @@ if (depth==0){
   int score;
   if (g.state!=RUNNING){
 
-    if (g.state==PLAYER1_WON) scor+e=-10;
+    if (g.state==PLAYER1_WON) score+=-10;
     else if (g.state==PLAYER2_WON) score+=10;
     else if (g.state==TIE) score+=0;
 
